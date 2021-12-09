513b1c71e273d0024f97a8ee7cacde5e8d828533
Soufiane Elkhamlichi <05soufiane@gmail.com>
2021-12-09 04:09:06 +0100

    Setting up the header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,25 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 23:26:18 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/12/05 23:26:19 by sel-kham         ###   ########.fr       */
+/*   Updated: 2021/12/09 03:40:54 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_GNL
+# define FT_GNL
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# define BUFFER_SIZE 42
+
+char	*get_next_line(int fd);
+int		ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char const *s2, int len);
+char	*ft_strdup(const char *s1);
+
+#endif
