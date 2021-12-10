b426ca65ee60be0761d2dbc57b94f0dbf9f4e77c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2021-12-10 03:06:26 +0100

    Updating the header file to much the new algorithm

--- get_next_line.h
+++ get_next_line.h
@@ -6,25 +6,24 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 23:26:18 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/12/09 03:40:54 by sel-kham         ###   ########.fr       */
+/*   Updated: 2021/12/10 02:55:22 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_GNL
-# define FT_GNL
+#ifndef GET_NEXT_LINE_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <stdlib.h>
-# define BUFFER_SIZE 42
+# define GET_NEXT_LINE_H
+
+# include<stdio.h>
+# include<unistd.h>
+# include<stdlib.h>
+# include<fcntl.h>
+# include<stdlib.h>
 
 char	*get_next_line(int fd);
 int		ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char *s1, char const *s2, int len);
+char	*ft_strjoin(char *s1, char *s2, int len);
 char	*ft_strdup(const char *s1);
 
 #endif
