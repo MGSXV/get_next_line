c3d06f0e99bba67ef9a370a56f7d3b12355aa547
Soufiane Elkhamlichi <05soufiane@gmail.com>
2021-12-10 03:07:19 +0100

    Updating ft_strlen, ft_strjoin and removing substr

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,17 +6,19 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 23:26:04 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/12/09 01:48:50 by sel-kham         ###   ########.fr       */
+/*   Updated: 2021/12/10 02:43:17 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "get_next_line.h"
+#include "get_next_line.h"
 
 size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
 	len = 0;
+	if (s == NULL)
+		return (0);
 	while (s[len])
 		len++;
 	return (len);
@@ -49,32 +51,12 @@ char	*ft_strdup(const char *s1)
 	return (target);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	s_len;
-	char	*str;
-
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (len > s_len - start)
-		len = s_len - start;
-	if (start >= s_len)
-		return (ft_strdup(""));
-	str = (char *) malloc((len + 1) * sizeof(char));
-	if (!str)
-		return (NULL);
-	ft_memcpy(str, s + start, len);
-	str[len] = '\0';
-	return (str);
-}
-
 int	ft_strchr(const char *s, int c)
 {
 	int		i;
 
 	i = -1;
-	if (!c)
+	if (!c || !s)
 		return (i);
 	while (s[++i])
 		if (s[i] == (char) c)
@@ -82,15 +64,15 @@ int	ft_strchr(const char *s, int c)
 	return (-1);
 }
 
-char	*ft_strjoin(char *s1, char const *s2, int len)
+char	*ft_strjoin(char *s1, char *s2, int len)
 {
 	char	*result;
 	size_t	s1_len;
 	size_t	s2_len;
 	size_t	i;
 
-	if (!s1 /*|| !s2*/)
-		return (ft_substr(s2, 0, len));
+	if (!s2)
+		return (s1);
 	s1_len = ft_strlen(s1);
 	s2_len = len;
 	i = -1;
@@ -101,5 +83,6 @@ char	*ft_strjoin(char *s1, char const *s2, int len)
 	while (++i < s2_len)
 		result[i + s1_len] = s2[i];
 	result[s1_len + s2_len] = '\0';
+	free(s1);
 	return (result);
 }
