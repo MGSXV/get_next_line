6e09dead35fdaee90d7fbdabecd090a1790ab8f2
Soufiane Elkhamlichi <05soufiane@gmail.com>
2021-12-10 03:06:41 +0100

    Using new iterative algorithm

--- get_next_line.c
+++ get_next_line.c
@@ -6,39 +6,66 @@
 /*   By: sel-kham <sel-kham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 23:25:42 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/12/09 03:53:34 by sel-kham         ###   ########.fr       */
+/*   Updated: 2021/12/10 03:02:14 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*ft_check_next_line(char *leftover)
+{
+	char	*res;
+	int		start;
+	int		i;
+
+	if (!leftover)
+		return (NULL);
+	i = -1;
+	start = ft_strchr(leftover, '\n');
+	if (start == -1)
+		return (ft_strdup(leftover));
+	res = malloc((start + 2) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (++i != start)
+		res[i] = leftover[i];
+	res[i] = '\n';
+	res[i + 1] = 0;
+	return (res);
+}
+
+static char	*ft_remove_last_line(char *leftover)
+{
+	int	start;
+
+	start = ft_strchr(leftover, '\n');
+	if (!leftover || start == -1 || ft_strlen(leftover) - start == 1)
+		return (free(leftover), NULL);
+	return (free(leftover), ft_strdup(leftover + start + 1));
+}
+
 char	*get_next_line(int fd)
 {
-	int			start;
 	int			res_len;
+	char		*tmp_buff;
 	char		*s;
-	static char	*temp_buff;
 	static char	*leftover;
 
-	s = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	tmp_buff = NULL;
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	s = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!s)
 		return (NULL);
-	s[BUFFER_SIZE] = 0;
-	res_len = read(fd, s, BUFFER_SIZE);
-	if (res_len == -1)
-		return (NULL);
-	if (!res_len)
-		return (ft_strdup(""));
-	start = ft_strchr(s, '\n');
-	if (start != -1)
+	while (ft_strchr(leftover, '\n') == -1)
 	{
-		temp_buff = ft_strjoin(leftover, s, start + 1);
-		leftover = ft_substr(s, start + 1, ft_strlen(s) - start);
-	}
-	else
-	{
+		res_len = read(fd, s, BUFFER_SIZE);
+		if (res_len <= 0)
+			break ;
+		s[res_len] = 0;
 		leftover = ft_strjoin(leftover, s, res_len);
-		get_next_line(fd);
 	}
-	return (temp_buff);
+	tmp_buff = ft_check_next_line(leftover);
+	leftover = ft_remove_last_line(leftover);
+	return (free(s), tmp_buff);
 }
